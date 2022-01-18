41df2505c04a80802d7da78895ba0700a1c9e127
f776655321 <75135259+f776655321@users.noreply.github.com>
2022-01-18 14:22:23 +0800

    Update axios.js

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_ROOT = "http://localhost:5000"
+const API_ROOT = "https://moosetherecordbackend.herokuapp.com/"
 
 const instance = axios.create({
     baseURL: API_ROOT,
